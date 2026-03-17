c64bd8ccb7ac2b613614aa808eba67fca119b74e
Rudransh Gawande <rudranshgawande007@gamil.com>
2026-03-17 09:06:00 +0530

    Fix missing import for ChangeNotifier in AuthService

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:crypto/crypto.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:progresso/services/mongodb_service.dart';
